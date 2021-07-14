931cbef2108f6e78de8859aa167b29b0d4ba7570
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2021-07-14 10:14:57 -0300

    Deploy Projeto Gama

---
+++ projetoGama/src/main/java/com/projetoGama/controller/UsuarioController.java
@@ -0,0 +1,78 @@
+package com.projetoGama.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoGama.model.UserLogin;
+import com.projetoGama.model.Usuario;
+import com.projetoGama.repository.UsuarioRepository;
+import com.projetoGama.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*" , allowedHeaders = "*")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> GetAll() {
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id){
+		return usuarioRepository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());				
+	}
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity <Usuario> Post(@RequestBody Usuario usuario) {
+		
+		Usuario usuarioResp = usuarioService.cadastrarUsuario(usuario);
+		try {
+			return ResponseEntity.status(HttpStatus.CREATED).body(usuarioResp);
+
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+
+	
+	@PutMapping("/alterar")
+	public ResponseEntity<Usuario> putUsuario(@RequestBody Usuario usuario){
+		Optional<Usuario> updateUsuario = usuarioService.atualizarUsuario(usuario);
+		try {
+			return ResponseEntity.ok(updateUsuario.get());
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+			}
+	}
+}
+	
+	
+
+
+
---
+++ projetoGama/src/main/java/com/projetoGama/model/Usuario.java
@@ -0,0 +1,193 @@
+package com.projetoGama.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	//@Size(min = 2, max = 100)
+	private String nomeCompleto;
+	
+	private String cep;
+	
+	private String numeroCasa;
+	
+	private String rne;
+	
+	//@Size(min = 13, max = 15)
+	private String telefone;
+	
+	@NotNull
+	@Email
+	private String login;
+	
+	@NotNull
+	//@Size(min = 8);
+	private String senha;
+	
+	private boolean empregador;
+	
+	private String nomeEmpresa;
+	
+	@CNPJ
+	private String cnpj;
+	
+	private String endereco;
+	
+	@Column(name = "dt_nascimento")
+	@JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate dataNascimento; // Atributo adicional
+	
+	
+	public Usuario() {}
+	
+	//Todos os atributos?
+	public Usuario(long id, String nomeCompleto, String login, String senha, LocalDate dataNascimento) {
+		this.id = id;
+		this.nomeCompleto = nomeCompleto;
+		this.login = login;
+		this.senha = senha;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	// remove: para que todas as postagens sejam excluidas junto com o usuario 
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getNumeroCasa() {
+		return numeroCasa;
+	}
+
+	public void setNumeroCasa(String numeroCasa) {
+		this.numeroCasa = numeroCasa;
+	}
+
+	public String getRne() {
+		return rne;
+	}
+
+	public void setRne(String rne) {
+		this.rne = rne;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public boolean isEmpregador() {
+		return empregador;
+	}
+
+	public void setEmpregador(boolean empregador) {
+		this.empregador = empregador;
+	}
+
+	public String getNomeEmpresa() {
+		return nomeEmpresa;
+	}
+
+	public void setNomeEmpresa(String nomeEmpresa) {
+		this.nomeEmpresa = nomeEmpresa;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+}
