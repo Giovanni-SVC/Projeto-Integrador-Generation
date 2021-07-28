7483b613d7025a0e3e32c898537478a31c03e27e
Djan Domingues <felipej.domingues@gmail.com>
2021-07-28 10:45:09 -0300

    Primeiros componentes do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    LoginComponent,
+    CadastroComponent,
+    HomeComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
