5ea3ca349ed05a74231570898df3fd454feb6f82
Jonathan da Silva <jonathansilva15sp@gmail.com>
2021-08-20 15:33:59 -0300

    view-curriculo

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -12,12 +12,17 @@ export class MenuComponent implements OnInit {
   nome = environment.nome
   token = environment.token
   id = environment.id
-
+  menu: string
   constructor(
     private router: Router
   ) { }
 
   ngOnInit() {
+    if(environment.tipoUsuario == 'normal'){
+      this.menu = "Vagas"
+    } else{
+      this.menu = "Curriculos"
+    }
   }
 
   sair(){
