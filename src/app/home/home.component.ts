a9d8e53eed1e24f4560fbcb68afc9e03f0e8090e
Jonathan da Silva <jonathansilva15sp@gmail.com>
2021-08-23 13:10:26 -0300

    Estilização Tazo: view, sobre, contato e home.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 
 @Component({
@@ -7,12 +8,27 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  nome = environment.nome
+  token = environment.token
+  id = environment.id
+  tipoUsuario = environment.tipoUsuario
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(){
     environment.token == ''
     window.scroll(0,0)
+    this.sair()
+  }
+
+  sair(){
+    this.router.navigate(['/home'])
+    environment.token = ''
+    environment.nome = ''
+    environment.id = 0
+    environment.tipoUsuario = ''
+
   }
 
 }
